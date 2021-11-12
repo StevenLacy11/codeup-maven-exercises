20a9bab4029426f2de915a5389f2ec2c69586f14
Steven Lacy <steven.lacy11@gmail.com>
2021-11-12 11:50:35 -0600

    completed StringExercise

---
+++ src/main/java/StringExercise.java
@@ -0,0 +1,23 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+class StringExercise {
+	public static void main(String[] args) {
+
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter Something: ");
+		String input = keyboard.nextLine();
+		System.out.println("You Entered: " + input);
+
+		if (StringUtils.isNumeric(input)) {
+			System.out.print(input + " is a number");
+		} else {
+			System.out.println(input + " is not a number");
+		}
+		System.out.println("Flipped Case: " + StringUtils.swapCase(input));
+		System.out.println("Reversed: " + StringUtils.reverse(input));
+	}
+}
+
+
